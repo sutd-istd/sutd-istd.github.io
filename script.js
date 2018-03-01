27d4d8563e89084b6688b6ccd1e51ee6049038ac
joel-huang <huang.joel@hotmail.com>
2018-03-02 04:14:11 +0800

    Revert "test5"
    This reverts commit 90dfa5c9aad09d1d8459ba186f0ab359282b03f4.

--- script.js
+++ script.js
@@ -64,23 +64,18 @@ function getDesc(id) {
   }
 }
 
-function detectMobile() { 
-
-  if (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1) {
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
     return true;
   }
-
-  else if(navigator.userAgent.match(/Android/i)
-  || navigator.userAgent.match(/webOS/i)
-  || navigator.userAgent.match(/iPhone/i)
-  || navigator.userAgent.match(/iPad/i)
-  || navigator.userAgent.match(/iPod/i)
-  || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i)
-  ) {
-    return true;
-  }
-  else {
+ else {
     return false;
   }
 }
