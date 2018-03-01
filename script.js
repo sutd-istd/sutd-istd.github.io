90dfa5c9aad09d1d8459ba186f0ab359282b03f4
joel-huang <huang.joel@hotmail.com>
2018-03-02 03:58:54 +0800

    test5

--- script.js
+++ script.js
@@ -64,18 +64,23 @@ function getDesc(id) {
   }
 }
 
-function detectmob() { 
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
+function detectMobile() { 
+
+  if (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1) {
     return true;
   }
- else {
+
+  else if(navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  }
+  else {
     return false;
   }
 }
